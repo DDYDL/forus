bd430d5967efd0e7460fa1bfe86c5f50e6a6d024
ldy <seven799117@gmail.com>
2024-09-03 15:55:14 +0900

    Delete dao

--- src/main/java/dao/recruit/recruitDao.java
+++
@@ -1,6 +0,0 @@
-package dao.recruit;
-
-public interface recruitDao {
-	//void insertRecruit_post(recruit_post post) throws Exception;
-	//recruit_post selectRecruit_post(Integer num) throws Exception;
-}
