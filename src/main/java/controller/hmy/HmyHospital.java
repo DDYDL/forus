d5a66da683aabcf5457ae02bb05f6dfe4a22e796
mel <codnjs9504@gmail.com>
2024-09-04 09:49:23 +0900

    Add Service, Controller

---
+++ src/main/java/controller/hmy/HmyHospital.java
@@ -0,0 +1,65 @@
+package controller.hmy;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dto.Hospital;
+
+/**
+ * Servlet implementation class HmyHospital
+ */
+@WebServlet("/hmyHospital")
+public class HmyHospital extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HmyHospital() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		Hospital hos = new Hospital();
+		hos.setH_id(Integer.parseInt(request.getParameter("h_id")));
+		hos.setUser_id(Integer.parseInt(request.getParameter("user_id")));
+		hos.setH_exponent_name(request.getParameter("h_exponent_name"));
+		hos.setH_num(Integer.parseInt(request.getParameter("h_num")));		
+		hos.setH_license(request.getParameter("h_license"));
+		hos.setH_name(request.getParameter("h_name"));
+		hos.setH_manager_name(request.getParameter("manager_name"));
+		 hos.setH_phone(request.getParameter("h_phone"));
+		 hos.setH_address(request.getParameter("h_address"));
+		 hos.setH_picture(request.getParameter("h_picture"));
+		 hos.setH_memo(request.getParameter("h_memo"));
+		 hos.setH_memo_road(request.getParameter("h_memo_road"));
+		 //hos.setH_pay(request.getParameterValues("h_pay"));
+		 hos.setH_sns(request.getParameter("h_sns"));
+		 //hos.setH_parking(request.getParameter("h_parking"));
+		 //hos.setH_isopen(request.getParameter("h_isopen"));
+		// hos.setH_lunch_time_start(request.getParameter("h_lunch_time_start"));
+		// hos.setH_lunch_time_end(request.getParameter("h_lunch_time_end"));
+		// hos.setH_interval_time(request.getParameter("h_interval_time"));
+
+		   
+
+	}
+
+}
