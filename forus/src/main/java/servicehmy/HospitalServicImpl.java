b74673105338e8f92c19552524bebe7671c847b9
mel <codnjs9504@gmail.com>
2024-09-09 14:31:05 +0900

    push in 0909

---
+++ forus/src/main/java/servicehmy/HospitalServicImpl.java
@@ -0,0 +1,20 @@
+package servicehmy;
+
+import dto.hospital;
+
+public class HospitalServicImpl implements HospitalService {
+
+	
+	@Override
+	public void join(hospital hos) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void login(String id, String password) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
